54a68a306d2bdb31ed5818fde308e1f1eb94ea24
loupers <loupers@matfyz.cz>
2022-12-18 12:21:34 +0100

    forest fire sampling with seed argument

--- src/nal/sampling.py
+++ src/nal/sampling.py
@@ -3,13 +3,20 @@ import random
 import numpy as np
 from collections import deque
 
-def forest_fire(G: nx.Graph, sample_size : int = 100, p : float = 0.4, max_hops : int = 10) -> nx.Graph:
+def forest_fire(G: nx.Graph, sample_size : int = 100, p : float = 0.4, max_hops : int = 10, seed : int = None) -> nx.Graph:
+    if seed:
+        random.seed(seed)
+        np.random.seed(seed)
+
     sample_nodes = set()
     visited_nodes = deque()
     all_nodes = set(G.nodes)
+    
     while len(sample_nodes) < sample_size:
         remaining = list(all_nodes.difference(sample_nodes))
-        start_node = random.choice(remaining)
+        if seed:
+            remaining.sort()
+        start_node = remaining[np.random.randint(0, len(remaining))]
         sample_nodes.add(start_node)
 
         queue = deque([start_node])
@@ -22,10 +29,13 @@ def forest_fire(G: nx.Graph, sample_size : int = 100, p : float = 0.4, max_hops 
             cur_node = queue.popleft()
             sample_nodes.add(cur_node)
 
-            unvisited_neighbours = set(G.neighbors(cur_node)).difference(sample_nodes)
+            unvisited_neighbours = list(set(G.neighbors(cur_node)).difference(sample_nodes))
+            if seed:
+                unvisited_neighbours.sort()
             number_of_burned_neighbours = min(len(unvisited_neighbours), np.random.geometric(p))
-            burned_neighbours = random.sample(unvisited_neighbours, number_of_burned_neighbours)
-            visited_nodes.extendleft(unvisited_neighbours.difference(set(burned_neighbours)))
+            random.shuffle(unvisited_neighbours)
+            burned_neighbours = unvisited_neighbours[:number_of_burned_neighbours]
+            visited_nodes.extendleft(set(unvisited_neighbours).difference(set(burned_neighbours)))
 
             for n in burned_neighbours:
                 if len(sample_nodes) >= sample_size:
